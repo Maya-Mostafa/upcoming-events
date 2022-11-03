31c3369c141e8228e54c8d3d31e329cde2666e1e
Maya-Mostafa <mayamostafa@gmail.com>
2022-11-03 10:03:17 -0400

    overriding eslint unnecessary rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,14 @@
 require('@rushstack/eslint-config/patch/modern-module-resolution');
 module.exports = {
   extends: ['@microsoft/eslint-config-spfx/lib/profiles/react'],
-  parserOptions: { tsconfigRootDir: __dirname }
+  parserOptions: { tsconfigRootDir: __dirname },
+  rules : {
+    "@microsoft/spfx/no-async-await": "off",
+    "@typescript-eslint/no-floating-promises" : "off",
+    "@typescript-eslint/no-unnecessary-type-constraint": "off",
+    "@typescript-eslint/explicit-function-return-type" : "off",
+    "@typescript-eslint/no-unnecessary-type-assertion" : "off",
+    "@typescript-eslint/typedef" : "off"
+  }
 };
+
