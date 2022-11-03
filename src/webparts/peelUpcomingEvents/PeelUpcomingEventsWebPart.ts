939b3dd4875b9debdf8c831d2eb693c52ee8e63f
Maya-Mostafa <mayamostafa@gmail.com>
2022-11-03 10:07:17 -0400

    adding wp properties

--- src/webparts/peelUpcomingEvents/PeelUpcomingEventsWebPart.ts
+++ src/webparts/peelUpcomingEvents/PeelUpcomingEventsWebPart.ts
@@ -3,35 +3,108 @@ import * as ReactDom from 'react-dom';
 import { Version } from '@microsoft/sp-core-library';
 import {
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
+  IPropertyPaneDropdownOption,
+  PropertyPaneDropdown,
+  PropertyPaneTextField,
+  PropertyPaneSlider,
+  PropertyPaneToggle
 } from '@microsoft/sp-property-pane';
+import { PropertyFieldNumber } from '@pnp/spfx-property-controls/lib/PropertyFieldNumber';
+// import { PropertyFieldPeoplePicker, IPropertyFieldGroupOrPerson, PrincipalType } from '@pnp/spfx-property-controls/lib/PropertyFieldPeoplePicker';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import { IReadonlyTheme } from '@microsoft/sp-component-base';
+import { SPHttpClient } from '@microsoft/sp-http';
 
 import * as strings from 'PeelUpcomingEventsWebPartStrings';
 import PeelUpcomingEvents from './components/PeelUpcomingEvents';
 import { IPeelUpcomingEventsProps } from './components/IPeelUpcomingEventsProps';
 
 export interface IPeelUpcomingEventsWebPartProps {
-  description: string;
-  
+  wpTitle: string;
+  eventsList: string;
+  siteUrl: string;
+  dateRange: string;
+  numEvents: number;
+  showPaging: boolean;
+  pageSize: number;
+  targetAudience: any;
 }
 
 export default class PeelUpcomingEventsWebPart extends BaseClientSideWebPart<IPeelUpcomingEventsWebPartProps> {
-
+  
   private _isDarkTheme: boolean = false;
   private _environmentMessage: string = '';
+ 
+  /* Loading Dpd with list names - Start */
+  private _lists: IPropertyPaneDropdownOption[];
+  private async _loadLists(): Promise<IPropertyPaneDropdownOption[]> {    
+    const listsTitle : any = [];
+    try {
+      // https://learn.microsoft.com/en-us/openspecs/sharepoint_protocols/ms-wssts/8bf797af-288c-4a1d-a14b-cf5394e636cf
+      const response = await this.context.spHttpClient.get(this.properties.siteUrl + `/_api/web/lists?$select=Title&$filter=BaseType eq 0 and BaseTemplate eq 106 and Hidden eq false`, SPHttpClient.configurations.v1);
+      if (response.ok) {
+        const results = await response.json();
+        if(results){
+          //console.log('results', results);
+          results.value.map((result: any)=>{
+            listsTitle.push({
+              key: result.Title,
+              text: result.Title
+            });
+          });
+          return listsTitle;
+        }
+      }
+    } catch (error) {
+      return error.message;
+    }
+  }
+  protected onPropertyPaneConfigurationStart(): void {
+    if (this._lists) {
+      this.render();  
+      return;
+    }
+    this.context.statusRenderer.displayLoadingIndicator(this.domElement, 'lists');
+    this._loadLists()
+      .then((listOptions: IPropertyPaneDropdownOption[]): void => {
+        this._lists = listOptions;
+        this.context.propertyPane.refresh();
+        this.context.statusRenderer.clearLoadingIndicator(this.domElement);        
+        this.render();       
+      });
+  } 
+  protected onPropertyPaneFieldChanged(propertyPath: string, oldValue: any, newValue: any): void {
+    if (propertyPath === 'listName' && newValue) {
+      // push new list value
+      super.onPropertyPaneFieldChanged(propertyPath, oldValue, newValue);
+      // refresh the item selector control by repainting the property pane
+      this.context.propertyPane.refresh();
+      // re-render the web part as clearing the loading indicator removes the web part body
+      this.render();      
+    }
+    else {
+      super.onPropertyPaneFieldChanged(propertyPath, oldValue, oldValue);
+    }
+  }
+  /* Loading Dpd with list names - End */
 
   public render(): void {
     const element: React.ReactElement<IPeelUpcomingEventsProps> = React.createElement(
       PeelUpcomingEvents,
       {
-        description: this.properties.description,
         isDarkTheme: this._isDarkTheme,
-        environmentMessage: this._environmentMessage,
         hasTeamsContext: !!this.context.sdks.microsoftTeams,
         userDisplayName: this.context.pageContext.user.displayName,
-        context: this.context
+        
+        wpTitle: this.properties.wpTitle,
+        context: this.context,
+        eventsList: this.properties.eventsList,
+        siteUrl: this.properties.siteUrl,
+        dateRange: this.properties.dateRange,
+        numEvents: this.properties.numEvents,
+        showPaging: this.properties.showPaging,
+        pageSize: this.properties.pageSize,
+        targetAudience: this.properties.targetAudience
       }
     );
 
@@ -83,17 +156,93 @@ export default class PeelUpcomingEventsWebPart extends BaseClientSideWebPart<IPe
       pages: [
         {
           header: {
-            description: strings.PropertyPaneDescription
+            description: 'A Peel web part that displays upcoming events from the calendar list in the site or another site.'
           },
           groups: [
             {
-              groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
+                PropertyPaneTextField('wpTitle', {
+                  label: 'Title',
+                  value: this.properties.wpTitle,
+                }),
+              ]
+            },
+            {
+              groupName: 'Events source',
+              groupFields: [
+                PropertyPaneTextField('siteUrl', {
+                  label: 'Site link',
+                  value: this.context.pageContext.web.serverRelativeUrl,
+                  description: 'By default, this is the current site URL. Please specify a different URL if needed.'
+                }),
+                PropertyPaneDropdown('eventsList', {
+                  label: 'List name',
+                  options: this._lists,
+                }),
+              ]
+            },
+            {
+              groupName: 'Display options',
+              groupFields: [
+                PropertyPaneDropdown('dateRange', {
+                  label: 'Date range',
+                  options: [
+                    { key: 'all', text: 'All upcoming events'},
+                    { key: 'thisWeek', text: 'This week' },
+                    { key: 'nextTwoWeeks', text: 'Next two weeks' },
+                    { key: 'thisMonth', text: 'This month' },
+                    { key: 'thisQuarter', text: 'This quarter' },
+                  ],
+                  selectedKey : 'all'
+                }),
+                PropertyFieldNumber('numEvents', {
+                  key: "numEvents",
+                  label: "Show up to this many events",
+                  description: "Minumum is 5, maximum is 500",
+                  value: this.properties.numEvents,
+                  maxValue: 500,
+                  minValue: 5,
+                  disabled: false
+                }),
+              ]
+            },
+            {
+              groupName: 'Paging options',
+              groupFields: [
+                PropertyPaneToggle('showPaging', {
+                  label: 'Show paging',
+                  onText: 'On',
+                  offText: 'Off',
+                  checked : false
+                }),
+                PropertyPaneSlider('pageSize', {
+                  label: 'Number of events per page',
+                  min: 5,
+                  max: 50,
+                  value: this.properties.pageSize,
+                  disabled: !this.properties.showPaging,
+                  step : 5,
+                  showValue: true,
                 })
               ]
-            }
+            },
+            // {
+            //   groupName: 'Target audience',
+            //   groupFields: [
+            //     PropertyFieldPeoplePicker('targetAudience', {
+            //       label: 'Who can see this webpart?',
+            //       initialData: this.properties.targetAudience,
+            //       allowDuplicate: false,
+            //       principalType: [PrincipalType.Users, PrincipalType.SharePoint, PrincipalType.Security],
+            //       onPropertyChange: this.onPropertyPaneFieldChanged,
+            //       context: this.context as any,
+            //       properties: this.properties,
+            //       onGetErrorMessage: null,
+            //       deferredValidationTime: 0,
+            //       key: 'peopleFieldId'
+            //     })
+            //   ]
+            // }
           ]
         }
       ]
